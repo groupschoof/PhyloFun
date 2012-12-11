b85ccc3eb62a052afca815e07fe2e14d52207b42
Asis Hallab <asis.hallab@mpipz.mpg.de>
2012-12-10 18:46:51 -0600

    Measurement of mutation probabilities now also works on combined distances.
    Added method to compute the euclidean distance from (0,0) to each pair
    of distances (seq.dist, das.dist). In order to compute p( 'mutation' |
    seq.dist, das.dist ) the distances.table is sorted by those euclidean
    distances.

--- src/measureDistanceFunctions.R
+++ src/measureDistanceFunctions.R
@@ -32,8 +32,12 @@ measureDistances <- function( annotation, annotation.matrix,
   #
   # Returns: a matrix where each row is labelled with the considered protein
   # pair (accessions in alphabetical order) and columns are sequence and
-  # domain architecture distance, respectively. Last column holds a boolean
-  # indicating wether the pairs shares the argument annotation.
+  # domain architecture distance, respectively. Another column holds a boolean
+  # indicating wether the pairs shares the argument annotation. The last column
+  # hold the pairwise euclidean distance of the distance pair coordinates to
+  # (0,0). The last column is used to sort the measured distances when
+  # computing the mutation probability depending on _both_ distances: p(
+  # "Mutation" | Sequence.Distance, Domain.Architecture.Distance )
   #   
   annot.annot.mtrx <- sharedAnnotation( annotation.matrix, annotation,
     annotation.type=annotation.type
@@ -62,12 +66,14 @@ measureDistances <- function( annotation, annotation.matrix,
       shrd.annot <- shareAnnotation( annotation, annotation.matrix,
         acc.a, acc.b
       )
+      eucl.dist <- dist( matrix( c(0, seq.dist, 0, das.dist), nrow=2 ) )[[1]]
       matrix(
-        c( seq.dist, das.dist, shrd.annot ), nrow=1,
+        c( seq.dist, das.dist, shrd.annot, eucl.dist ), nrow=1,
         dimnames=list(
           sortedPairName( acc.a, acc.b ),
           c( "Sequence.Distance", "Domain.Architecture.Distance",
-            paste("Share.", annotation, sep="")
+            paste("Share.", annotation, sep=""),
+            "Euclidean.Distance.To.Origin"
           )
         )
       )
@@ -75,36 +81,120 @@ measureDistances <- function( annotation, annotation.matrix,
   )
 }
 
-mutationProbabilityDistribution <- function( distances.tbl, 
-  p.column.name, dep.column.name, 
-  p.grid=seq(0.1,1,by=0.1)
-  ) {
-  p.mut.colname <- paste( "p.mutation|", p.column.name, sep="" )
+pMutation <- function( pairs.shared, pairs.diff, min.p.mut=0 ) {
+  # Computes the fraction pairs.diff / ( pairs.shared + pairs.diff ).
+  #
+  # Args:
+  #  pairs.shared : The number of protein pairs sharing a certain annotation. 
+  #  pairs.diff : The number of protein pairs NOT sharing a certain annotation. 
+  #  min.p.mut : The minimum of the computed p.mutation to return.
+  #
+  # Returns: The computed fraction or zero, if denominator is zero. If computed
+  # fraction is smaller than argument 'min.p.mut' min.p.mut is returned.
+  #   
+  denominator <- pairs.shared + pairs.diff
+  p.mut <- if ( denominator == 0 ) 0 else pairs.diff / denominator
+  if ( p.mut < min.p.mut ) min.p.mut else p.mut
+}
 
-  srtd <- distances.tbl[ sort.list( distances.tbl[ , p.column.name ] ), ]
-  pairs.sharing <- 0; pairs.diff <- 0; row.ind <- 1; p.mut.last <- 0;
-  p.mut.mtrx <- do.call('rbind',
-    lapply( 1:nrow(srtd), function(i) {
-      if ( srtd[[ i, 3 ]] ) {
+mutationProbabilityDistribution <- function( distances.tbl, p.column.name,
+  annotation.shared.column=3, lapply.funk=lapply ) {
+  # Measure function mutation probability depending on argument column
+  # 'p.column.name': p( 'Function.Mutation' | 'p.mut.colname' ). This
+  # probability is measured by sorting the argument matrix 'distances.tbl' by
+  # argument column 'p.column.name' and iteratively calculating p given how
+  # many pairs sharing and not sharing the current function annotation have
+  # been found. This computation only returns increasing values for p, if pairs
+  # with a greater value in 'p.column.name' are found and thus p would decrease
+  # for this pair, the last and higher value is used as p for the current pair
+  # again.
+  # 
+  # Note: See function 'measureDistances' for more details on the structure of
+  #       argument 'distances.tbl'.
+  #
+  # Args:
+  #  distances.tbl : A matric containing distance measures (columns) for pairs
+  #                  of proteins (rows), as i.e. returned by function
+  #                  'measureDistances'.
+  #  p.column.name : The distance measure on which to base the computation of
+  #                  p( 'Function.Mutation' ).
+  #  annotation.shared.column : The distances.tbl's column in which to look for
+  #                             the boolean 'Does this pair share the
+  #                             annotation, distances.tbl was computed for?'
+  #                             (default=3) 
+  #  lapply.funk : Set to mclapply, if parallel computation is wanted.
+  #
+  # Returns: A sorted copy of argument matrix 'distances.tbl' with an
+  # additional column holding the measured annotation mutation probabilities
+  # for each row. Sorting is done on argument column 'p.mut.colname'.
+  #   
+  p.mut.colname <- paste( "p.mutation|", p.column.name, sep="" )
+  srtd <- distances.tbl[ sort.list( distances.tbl[ , p.column.name ] ), ,
+    drop=F ]
+  pairs.sharing <- 0; pairs.diff <- 0; p.mut.last <- 0;
+  do.call( 'rbind',
+    lapply.funk( 1:nrow(srtd), function(i) {
+      if ( srtd[[ i, annotation.shared.column ]] ) {
         pairs.sharing <<- pairs.sharing + 1
       } else {
         pairs.diff <<- pairs.diff + 1
       }
-      p.mut <- if( pairs.diff > pairs.sharing ) 1 else pairs.diff / pairs.sharing
-      
-      # Mutation probability can not decline with increasing distances:
-      if ( p.mut < p.mut.last )
-        p.mut <- p.mut.last
+      p.mut <- pMutation( pairs.sharing, pairs.diff, p.mut.last )
+      # Mutation probability must not decline with increasing distances:
       p.mut.last <<- p.mut
 
-      setNames(
-        list( p.mut, srtd[[ i, p.column.name ]], srtd[[ i, dep.column.name ]], srtd[[ i, 3 ]] ),
-        list( p.mut.colname, p.column.name, dep.column.name, colnames( srtd )[3] )
+      matrix(
+        c( p.mut, srtd[ i, ] ),
+        nrow=1, dimnames=list(
+          rownames( srtd )[ i ],
+          c( p.mut.colname, colnames( srtd ) )
+        )
       )
     })
   )
-  rownames( p.mut.mtrx ) <- rownames( srtd )
+}
+
+measureEuclideanDists <- function( dists.mtrx, dists.to.coordinates=c( 0,0 ),
+  dists.mtrx.columns=c( 1,2 ), lapply.funk=lapply ) {
+  # Measures pairwise euclidean distances to argument 'dists.to.coordinates'
+  # for each row of argument 'dists.mtrx', coordinates are extracted using
+  # indices defined in argument 'dists.mtrx.columns'. 
+  #
+  # Args:
+  #  dists.mtrx : Matrix, where each row is a set of euclidean coordinates.
+  #               I.e. as returned by function 'measureDistances'.
+  #  dists.to.coordinates : Measure distances to this point in vector space.
+  #  dists.mtrx.columns : The column indices of the argument 'dists.mtrx' to
+  #                       measure euclidean distances with.
+  #  lapply.funk : Set to 'mclapply' if computation is wanted in parallel.
+  #
+  # Returns: An extended copy of the argument matrix 'dists.mtrx' with an
+  # additional column holding the pairwise euclidean distances to argument
+  # 'dists.to.coordinates'. 
+  #   
+  paste.funk <- function( ... ) { paste( ..., sep="." ) }
+  euc.dist.col <- do.call( 'paste.funk',
+    as.list( c( "Euclidean.Distance.To", dists.to.coordinates ) )
+  )
   
-  # return
-  p.mut.mtrx
+  do.call( 'rbind',
+    lapply.funk( 1:nrow(dists.mtrx), function(i) {
+      matrix( c( dists.mtrx[ i, ],
+          dist(
+            matrix( 
+              c(
+                dists.mtrx[ i, dists.mtrx.columns ],
+                dists.to.coordinates
+              ), nrow=2, byrow=T
+            )
+          )[[1]]
+        ),
+        nrow=1,
+        dimnames=list(
+          rownames(dists.mtrx)[[i]],
+          c( colnames(dists.mtrx), euc.dist.col )
+        ) 
+      )
+    })
+  )
 }
--- test/testMeasureDistanceFunctions.R
+++ test/testMeasureDistanceFunctions.R
@@ -47,41 +47,66 @@ domain.weights.table <- read.table( project.file.path( "test",
 go.term <- "GO:0017111"
 dists <- measureDistances( go.term, annos, blast.rslt.tbl, aa.seqs,
   domain.weights.table )
-print( dists )
+# print( dists )
 checkTrue( ! is.null( dists ) )
 checkEquals( class(dists), 'matrix' )
 checkEquals( nrow(dists), 4 )
 exp.dists <- matrix(
   c(
-    1.13, 0.56, T,
-    1.13, 0.00, T,
-    0.67, 0.32, 0,
-    0.79, 1.00, 0
+    1.13, 0.56, T, 1.26,
+    1.13, 0.00, T, 1.13,
+    0.67, 0.32, 0, 0.74,
+    0.79, 1.00, 0, 1.27
   ),
   byrow=T, nrow=4,
   dimnames=list(
     c( "A0PKB2_A0RLX8", "A0K2M8_A0RLX8", "A0K2M8_A0KR35", "A0KEC3_A0Q3U6" ),
-    c( "Sequence.Distance", "Domain.Architecture.Distance", "Share.GO:0017111")
+    c( "Sequence.Distance", "Domain.Architecture.Distance", "Share.GO:0017111", "Euclidean.Distance.To.Origin" )
+
   )
 )
-checkEquals( dists, exp.dists )
+checkEquals( round( dists, 2 ), exp.dists )
+
+# Test pMutation
+print("Testing pMutation(...)")
+checkEquals( pMutation( 0, 0 ), 0 )
+checkEquals( pMutation( 3, 0 ), 0 )
+checkEquals( pMutation( 0, 3 ), 1 )
+checkEquals( pMutation( 3, 1, 0.5 ), 0.5 )
 
 # Test mutationProbabilityDistribution
 print("Testing mutationProbabilityDistribution(...)")
-dists.test <- matrix( c(0.1, 0.3, 0.6, 1.0, 0.1, 0.3, 0.6, 1.0, 1, 1, 0, 1),
-  nrow=4, ncol=3, dimnames=list(
-    c("A_B", "A_C", "B_C", "C_D"),
-    c("Sequence.Distance", "Domain.Architecture.Distance", "Share.GO:7272727")
+dists.test <- matrix( c( 0.1, 0.3, 0.6, 1.0, 0.1, 0.6, 0.3, 1.0, 1, 1, 0, 1, 1.26, 1.13, 0.74, 1.27 ),
+  nrow=4, ncol=4, dimnames=list(
+    c( "A_B", "A_C", "B_C", "C_D" ),
+    c( "Sequence.Distance", "Domain.Architecture.Distance", "Share.GO:7272727", "Euclidean.Distance.To.Origin" )
   )
 )
-p.mut.seq <- mutationProbabilityDistribution( dists.test, "Sequence.Distance", "Domain.Architecture.Distance" )
-f <- file( project.file.path( "test", "p_mutation_sequence_distance_R_serialized.txt" ), "r" )
-exp.p.mut.seq <- unserialize( f )
-close( f )
-checkEquals( p.mut.seq, exp.p.mut.seq )
+# print( dists.test )
+p.mut.seq <- mutationProbabilityDistribution( dists.test, "Sequence.Distance" )
+# print( p.mut.seq )
+exp.p.mut.seq <- matrix( c( 0, 0, 0.33, 0.33 ), ncol=1,
+  dimnames=list(
+    c( "A_B", "A_C", "B_C", "C_D" ),
+    "p.mutation|Sequence.Distance"
+  )
+)
+checkEquals( round( p.mut.seq[ , "p.mutation|Sequence.Distance", drop=F ], 2 ),
+  exp.p.mut.seq
+)
+checkEquals( p.mut.seq[ , 2:ncol(p.mut.seq) ], dists.test )
 
-p.mut.das <- mutationProbabilityDistribution( dists.test, "Domain.Architecture.Distance", "Sequence.Distance" )
-f <- file( project.file.path( "test", "p_mutation_das_distance_R_serialized.txt" ), "r" )
-exp.p.mut.das <- unserialize( f )
-close( f )
-checkEquals( p.mut.das, exp.p.mut.das )
+p.mut.das <- mutationProbabilityDistribution( dists.test, "Domain.Architecture.Distance" )
+# print( p.mut.das )
+exp.p.mut.das <- matrix( c( 0, 0.5, 0.5, 0.5 ), ncol=1,
+  dimnames=list(
+    c( "A_B", "B_C", "A_C", "C_D" ),
+    "p.mutation|Domain.Architecture.Distance"
+  )
+)
+checkEquals( p.mut.das[ , "p.mutation|Domain.Architecture.Distance", drop=F ],
+  exp.p.mut.das
+)
+checkEquals( p.mut.das[ , 2:ncol(p.mut.das) ],
+  dists.test[ sort.list( dists.test[ , "Domain.Architecture.Distance" ] ), ]
+)
