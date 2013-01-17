bfdf49cd66c371c8b62cd9339388251ea141dccb
Asis Hallab <asis.hallab@mpipz.mpg.de>
2013-01-17 16:42:41 -0600

    Added function to measure fScore

--- src/evaluation.R
+++ src/evaluation.R
@@ -32,3 +32,23 @@ recall <- function( predicted.gos, true.gos ) {
   tp <- intersect( tgs, pgs )
   if ( length( tgs ) == 0 ) 1 else length( tp ) / length( tgs )
 }
+
+fScore <- function( predicted.gos, true.gos, beta.param=1 ) {
+  # Computes the weighted harmonic mean of precision and recall as a quality
+  # measure of predicted.gos. The higher beta.param the more emphasis is put on
+  # recall than on precision.
+  #
+  # Args:
+  #  predicted.gos : The set of predicted GO terms
+  #  true.gos      : The set of experimentally verified GO terms.
+  #  beta.param    : The factor of how much more emphasis should be put on
+  #                  recall than on precision.
+  #
+  # Returns: The weighted harmonic mean of precision and recall as a numeric
+  # between Zero and One.
+  #   
+  prcsn <- precision( predicted.gos, true.gos )
+  rcll <- recall( predicted.gos, true.gos )
+  bp <- beta.param^2
+  ( 1 + bp ) * ( prcsn * rcll ) / ( bp * prcsn + rcll )
+}
