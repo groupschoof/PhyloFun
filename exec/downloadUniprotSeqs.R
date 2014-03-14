0f05447cf1a258a3d2f4a518003b477e4b30782f
Asis Hallab <hallab@mpipz.mpg.de>
2014-03-14 12:37:26 +0100

    Added list of all UniprotKB accessions of proteins with trusted GO annotations. Also made script downloadUniprotSeqs.R download exactly these sequences by default, if no file with other accessions is provided.

--- exec/downloadUniprotSeqs.R
+++ exec/downloadUniprotSeqs.R
@@ -1,29 +1,16 @@
-library(tools)
-library(RCurl)
-library(XML)
-
-# In R sourcing other files is not trivial, unfortunately.
-# WARNING:
-# This method ONLY works for project files in depth one sub dirs!
-project.file.path <- function(...) {
-  initial.options <- commandArgs(trailingOnly = FALSE)
-  file.arg.name <- "--file="
-  script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
-  script.dir <- dirname(file_path_as_absolute(script.name))
-  project.dir <- sub(basename(script.dir),'',script.dir)
-  normalizePath(file.path(project.dir,...))
-}
-src.project.file <- function(...) {
-  source(project.file.path(...))
-}
-src.project.file( 'src','loadUniprotKBEntries.R' )
+require( PhyloFun )
 
 input.args <- commandArgs( trailingOnly = TRUE )
 
-print("Usage: Rscript downloadUniprotSeqs.R path/2/accession_per_line.txt path/2/output.fasta")
+print("Usage: Rscript downloadUniprotSeqs.R path/2/output.fasta [ path/2/accession_per_line.txt ] ")
 
 # Read input
-accs <- scan( input.args[[ 1 ]], what=character(), sep="\n" )
+accs <- if ( length( input.args ) > 1 ) {
+  scan( input.args[[ 2 ]], what=character(), sep="\n" ) 
+} else {
+  data( "ukb_proteins_with_trusted_go_annos", package = "PhyloFun" )
+  UKB_PROTEINS_WITH_TRUSTED_GO_ANNOS
+}
 
 # Download:
-downloadSequences( accs, input.args[[ 2 ]] )
+downloadSequences( accs, input.args[[ 1 ]] )
